10f171db81e92bd7ffda29164420aa843ac9aa73
최상열 <1-blue@anipen.com>
2022-11-17 18:34:57 +0900

    17-1

--- src/main/java/com/hello/core/discount/RateDiscountPolicy.java
+++ src/main/java/com/hello/core/discount/RateDiscountPolicy.java
@@ -3,12 +3,12 @@ package com.hello.core.discount;
 import com.hello.core.member.Grade;
 import com.hello.core.member.Member;
 
-public class FixDiscountPolicy implements DiscountPolicy{
-    private int discountFixAmount = 1000;
+public class RateDiscountPolicy implements DiscountPolicy{
+    private int discountPercent = 10;
     @Override
     public int discount(Member member, int price) {
         if (member.getGrade() == Grade.VIP){
-            return discountFixAmount;
+            return price * discountPercent / 100;
         }else {
             return 0;
         }
--- src/test/java/com/hello/core/member/MemberServiceTest.java
+++ src/test/java/com/hello/core/member/MemberServiceTest.java
@@ -1,10 +1,18 @@
 package com.hello.core.member;
 
+import com.hello.core.AppConfig;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class MemberServiceTest {
-    MemberService service = new MemberServiceImpl();
+    MemberService service;
+
+    @BeforeEach
+    public void beforeEach() {
+        service = new AppConfig().memberService();
+    }
+
     @Test
     void join(){
         // given
