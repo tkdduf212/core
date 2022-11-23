af55ebf1bfd190f4e7b64bc8bc98750f8b158f8d
최상열 <1-blue@anipen.com>
2022-11-23 17:52:36 +0900

    221123

--- src/main/java/com/hello/core/order/OrderServiceImpl.java
+++ src/main/java/com/hello/core/order/OrderServiceImpl.java
@@ -3,7 +3,6 @@ package com.hello.core.order;
 import com.hello.core.discount.DiscountPolicy;
 import com.hello.core.member.Member;
 import com.hello.core.member.MemberRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -11,7 +10,6 @@ public class OrderServiceImpl implements OrderService{
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
-    @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
--- src/test/java/com/hello/core/scan/AutoAppConfigTest.java
+++ src/test/java/com/hello/core/scan/AutoAppConfigTest.java
@@ -1,8 +1,10 @@
 package com.hello.core.scan;
 
 import com.hello.core.AutoAppConfig;
+import com.hello.core.member.MemberRepository;
 import com.hello.core.member.MemberService;
 import com.hello.core.member.MemberServiceImpl;
+import com.hello.core.order.OrderServiceImpl;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -17,5 +19,9 @@ public class AutoAppConfigTest {
 
         MemberService memberService = ac.getBean(MemberService.class);
         assertThat(memberService).isInstanceOf(MemberService.class);
+
+        OrderServiceImpl bean = ac.getBean(OrderServiceImpl.class);
+        MemberRepository memberRepository = bean.getMemberRepository();
+        System.out.println("memberRepository = " + memberRepository);
     }
 }
